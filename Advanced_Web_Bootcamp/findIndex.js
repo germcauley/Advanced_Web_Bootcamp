453f9affbe5ca4d291de394a8e5f3f0be7861208
germcauley <geraldmcauley@gmail.com>
2018-09-17 22:02:09 +0100

    findIndex script added

---
+++ Advanced_Web_Bootcamp/findIndex.js
@@ -0,0 +1,9 @@
+function findIndex(arr, callback) {
+    for(var i=0; i <arr.length; i++){
+        if (callback(arr[i],i,arr)){
+            return i;
+            }
+        }
+        return -1;
+            
+}
---
+++ Javascript.js
@@ -0,0 +1,5 @@
+var arr = [1,2,3,4,5,6];
+
+forEach(arr, function(num){
+    console.log(num *2)
+});
