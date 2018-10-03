e720404c2a9d61f8dfd52d07b346fca7b88fe09a
Gerald McAuley <geraldmcauley@gmail.com>
2018-10-03 23:08:01 +0100

    added currency buttons

--- BitcoinPriceAJAX/script.js
+++ BitcoinPriceAJAX/script.js
@@ -1,22 +1,65 @@
   var btn = document.querySelector("#btn");
   var span = document.querySelector("#price");
+  var eur = document.querySelector("#eur");
+  var usd = document.querySelector("#usd");
+  var gbp = document.querySelector("#gbp");
   
   function loadFunction() {
-    makeRequest();
+    makeEURRequest();
 }
+
+
   
   btn.addEventListener("click", function(){
     //make request
     makeRequest();
   });
+  eur.addEventListener("click", function(){
+    //make request
+    makeEURRequest();
+  });
+  usd.addEventListener("click", function(){
+    //make request
+    makeUSDRequest();
+  });
+  gbp.addEventListener("click", function(){
+    //make request
+    makeGBPRequest();
+  });
 
-  function makeRequest (){
+  function makeEURRequest (){
     var XHR = new XMLHttpRequest();
     XHR.onreadystatechange = function(){
       if(XHR.readyState ==4 && XHR.status ==200){
         var data = JSON.parse(XHR.responseText);
         var price = data.bpi.EUR.rate;
         span.innerText = price + " EUR";
+      }
+    }
+     XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
+    XHR.send();
+  }
+  
+  function makeUSDRequest (){
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function(){
+      if(XHR.readyState ==4 && XHR.status ==200){
+        var data = JSON.parse(XHR.responseText);
+        var price = data.bpi.USD.rate;
+        span.innerText = price + " USD";
+      }
+    }
+     XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
+    XHR.send();
+  }
+
+  function makeGBPRequest (){
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function(){
+      if(XHR.readyState ==4 && XHR.status ==200){
+        var data = JSON.parse(XHR.responseText);
+        var price = data.bpi.GBP.rate;
+        span.innerText = price + " GBP";
       }
     }
      XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
