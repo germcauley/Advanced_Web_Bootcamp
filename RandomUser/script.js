10755edb98a6a1b38fad6e74e544776b7a25665d
Gerald McAuley <geraldmcauley@gmail.com>
2018-10-10 23:17:00 +0100

    Working Random User Generator

--- RandomUser/script.js
+++ RandomUser/script.js
@@ -1,4 +1,10 @@
 var btn = document.querySelector("button");
+var avatar = document.querySelector("#avatar");
+var fullname = document.querySelector("#fullname");
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var city = document.querySelector("#city");
+
 btn.addEventListener("click", function(){
   var url = 'https://randomuser.me/api/';
   fetch(url)
@@ -6,8 +12,23 @@ btn.addEventListener("click", function(){
         return res.json();
     })
     .then(function(data){
-        var fullname = data.results[0].name.first;
-        console.log(fullname);
+        var firstname = data.results[0].name.first;
+        var lastname = data.results[0].name.last;
+        var usernametxt = data.results[0].login.username;
+        var emailtxt = data.results[0].email;
+        var citytxt = data.results[0].location.city;
+        var pic = data.results[0].picture.thumbnail;
+        console.log(firstname);
+        console.log(lastname);
+        console.log(username);
+        console.log(email);
+        console.log(city);
+        console.log(pic);
+        avatar.src=pic;
+        fullname.innerText = firstname+" "+lastname;
+        username.innerText = usernametxt;
+        email.innerText = emailtxt;
+        city.innerText = citytxt;
         
     })
     .catch(function(err){
