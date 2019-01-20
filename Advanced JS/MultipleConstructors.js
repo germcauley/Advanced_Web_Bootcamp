1f6ea61498336a1e20a417615a013dbb1a753d07
Gerald McAuley <geraldmcauley@gmail.com>
2019-01-20 19:30:29 +0000

    Add files via upload

--- Advanced JS/MultipleConstructors.js
+++ Advanced JS/MultipleConstructors.js
@@ -3,3 +3,26 @@ function Car(make, model,year){
     this.model = model;
     this.year = year;
 }
+
+
+function Person (firstName, lastName, favouriteColor, favouriteNumber){
+        this.firstName =firstName;
+        this.lastName=lastName;
+        this.favouriteColor=favouriteColor;
+        this.favouriteNumber=favouriteNumber;
+        this.multiplyFavouriteNumber= function(num){
+            return num * this.favouriteNumber;
+        }
+}
+
+function Parent(firstName, lastName, favoriteColor, favoriteFood){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteFood = favoriteFood;
+}
+
+function Child(firstName, lastName, favoriteColor, favoriteFood){
+   Parent.apply(this,arguments);
+   //Parent.call(this,firstName, lastName, favoriteColor, favoriteFood);
+}
